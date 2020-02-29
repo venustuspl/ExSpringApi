13aa567750a12e2dd0b66bfdf5f4343d52d7a3f5
venustuspl <venustus.pl@gmail.com>
2020-02-29 23:02:30 +0100

    0.0.3

---
+++ src/main/java/pl/venustus/ExSpringApi/app/Hello.java
@@ -0,0 +1,13 @@
+package pl.venustus.ExSpringApi.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping
+    public String main() {
+        return "Hello";
+    }
+}
