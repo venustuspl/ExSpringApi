647fbb49cf087b5c7b15a1ba1c87b627d683c3ca
venustuspl <venustus.pl@gmail.com>
2020-02-29 22:37:32 +0100

    0.0.1

---
+++ src/main/java/pl/venustus/ExSpringApi/ExSpringApiApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.ExSpringApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/ExSpringApi/ExSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.ExSpringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
